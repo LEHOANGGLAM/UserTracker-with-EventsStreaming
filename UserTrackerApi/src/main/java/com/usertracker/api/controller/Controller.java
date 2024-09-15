bee9fc9d8895d6adf431c228679312483de2ea4e
LEHOANGGLAM <lam.lecamhoang@unified.com>
2024-09-15 15:01:31 +0700

    refactor api repo

--- UserTrackerApi/src/main/java/com/usertracker/api/controller/Controller.java
+++ UserTrackerApi/src/main/java/com/usertracker/api/controller/Controller.java
@@ -15,23 +15,29 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
     private static final String OUTPUT_TOPIC = "user_clicks";
     private static final String BOOTSTRAP_SERVERS = "localhost:9092";
+    private KafkaProducer<String, String> producer;
+
+    public Controller() {
+        this.producer = createKafkaProducer();
+    }
 
     @GetMapping("/{userId}/products/{productId}")
     public ResponseEntity<String> getProductId(@PathVariable String userId, @PathVariable String productId) {
-        // Configure the producer
+        // Produce the updated count to the output topic
+        String event = String.format("User %s visited %s", userId, productId);
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<>(OUTPUT_TOPIC, null, event);
+        producer.send(producerRecord);
+
+        return ResponseEntity.ok(productId + "\nThis is the description");
+    }
+
+    // Method to configure and create Kafka producer
+    private KafkaProducer<String, String> createKafkaProducer() {
         Properties producerProps = new Properties();
         producerProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
         producerProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         producerProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
-        // Create Kafka producer
-        KafkaProducer<String, String> producer = new KafkaProducer<>(producerProps);
-
-        // Produce the updated count to the output topic
-        String event = String.format("User %s visited %s", userId, productId);
-        ProducerRecord<String, String> producerRecord = new ProducerRecord<>(OUTPUT_TOPIC, null, String.valueOf(event));
-        producer.send(producerRecord);
-
-        return ResponseEntity.ok(productId + "\nThis is the discription");
+        return new KafkaProducer<>(producerProps);
     }
 }
